f838d5ba5137b8f4329390102936bd2c6d1f883c
winters_huang <winters_huang>
2020-06-07 18:02:24 +0800

    add "sudo" to "cp rc.local_gpio_fix /etc/rc.local",fix Permission denied

--- rc_gpio_fix.sh
+++ rc_gpio_fix.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+                         #!/bin/bash -x
 
 mount -o remount,rw /
 
@@ -6,9 +6,9 @@ cd /tmp
 
 curl -OL https://github.com/bi7jta/nano_hotspot_upgrade_gpio_fix/raw/master/rc.local_gpio_fix
   
-chmod +x rc.local_gpio_fix
+sudo chmod +x rc.local_gpio_fix
 
-cp rc.local_gpio_fix /etc/rc.local
+sudo cp rc.local_gpio_fix /etc/rc.local
 
 echo "Done, now reboot ..."
 reboot
